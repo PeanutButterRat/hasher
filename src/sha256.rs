13048c2809c623b8de034fcb3864f70bdcf53e65
Eric Brown <ebrown5676@gmail.com>
2024-07-10 12:58:24 -0700

    Add unit tests for sha256

--- src/sha256.rs
+++ src/sha256.rs
@@ -173,3 +173,38 @@ fn sigma0(x: u32) -> u32 {
 fn sigma1(x: u32) -> u32 {
     rotr(x, 17) ^ rotr(x, 19) ^ shr(x, 10)
 }
+
+// Output was tested against the SHA tool from https://emn178.github.io/online-tools/sha256.html.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn abc() {
+        let message = "abc".as_bytes().to_vec();
+        let expected = "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad";
+        let actual = hash(message);
+
+        assert_eq!(hex::encode(actual), expected)
+    }
+
+    #[test]
+    fn alphabet() {
+        let message = "abcedefghijklmnopqrstuvwxyz".as_bytes().to_vec();
+        let expected = "c4fdd9f0589d29bdbf2603e01901380cc3bc9c101dd62ea8da558bc9c516352e";
+        let actual = hash(message);
+
+        assert_eq!(hex::encode(actual), expected)
+    }
+
+    #[test]
+    fn random() {
+        let message = "6d1e72ad03ddeb5de891e572e2396f8da015d899ef0e79503152d6010a3fe6916d1e72a\
+            d03ddeb5de891e572e2396f8da015d899ef0e79503152d6010a3fe6916d1e72ad03ddeb5de891e572e2396f8\
+            da015d899ef0e79503152d6010a3fe691".as_bytes().to_vec();
+        let expected = "870b0b44c38b67c4f184fd699677c2a5f0b18f95c1360c95fe2b946695f45c04";
+        let actual = hash(message);
+
+        assert_eq!(hex::encode(actual), expected)
+    }
+}

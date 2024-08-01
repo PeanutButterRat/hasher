5926533fe945183bdd12b85d77a9a7618dc05cfe
Eric Brown <ebrown5676@gmail.com>
2024-08-01 14:15:40 -0700

    Add sha512 to command line options

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ fn main() {
 
     let algorithm = match cli.algorithm.to_ascii_lowercase().as_str() {
         "sha256" => sha256::hash,
+        "sha512" => sha512::hash,
         _ => {
             println!("unknown algorithm specified ({})", cli.algorithm);
             exit(1);
